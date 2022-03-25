5050fa2069113ce6d459151668c58ba394d4515d
DrDano <danielharned@gmail.com>
2022-03-25 09:06:35 -0700

    adding projects to the portfolio component and setting href to deployment or walkthrough in the title

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,16 +10,32 @@ export default function Portfolio(props) {
       projectDeployment: "https://frognetwork.herokuapp.com/",
       key: 0,
       projectRepo: "https://github.com/DrDano/Blog-Frog"
-    }
+    },
+    {
+      title: "The Allergy-Free Kitchen",
+      description: "The Allergy-Free Kitchen is the app for you if you've ever been to a restaurant, taken that first bite and realized you've been poisoned yet again. The app is full of user-created recipes that list real ingredients and allergies together. Peruse around and take in all the smells and flavors.",
+      photo: "AFK.jpg",
+      projectDeployment: "https://afk-network.herokuapp.com/",
+      key: 1,
+      projectRepo: "https://github.com/JosephJamesCoop/the-allergy-free-kitchen"
+    },
+    {
+      title: "Inter-App",
+      description: "inter-App lends itself to any application looking to incorporate users, comments, and friends into their app. This is a simple-to-use design with a lot of power behind the curtain.",
+      photo: "interApp.jpg",
+      projectDeployment: "https://drive.google.com/file/d/1QQ-1rEx9wDw4cKdFj79J6T4CsCGT1IEI/preview",
+      key: 2,
+      projectRepo: "https://github.com/DrDano/inter-App"
+    },
   ])
 
   return (
     <>
       {projects.map((project, i) => (
         <div key={`div${project.key}`} className="p-2">
-        <h2 key={`h${project.key}`}>{project.title}</h2>
+        <h2 key={`h${project.key}`}><a href={project.projectDeployment} rel="noopener noreferrer" target="_blank">{project.title}</a></h2>
         <p key={`p${project.key}`}>{project.description}</p>
-        <img src={require(`../../assets/${project.photo}`)} href={project.projectDeployment} alt={project.title} key={`img${project.key}`} width="500" height="300"/>
+        <img src={require(`../../assets/${project.photo}`)} alt={project.title} key={`img${project.key}`} width="500" height="300"/>
         <a href={project.projectRepo} key={`anchor${project.key}`}>Project GitHub Repository</a>
         </div>
       ))}
