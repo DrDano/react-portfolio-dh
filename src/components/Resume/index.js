b8eff02eeac34342793e322007caf79d4eb78170
DrDano <danielharned@gmail.com>
2022-03-25 21:09:39 -0700

    adding link to downloadable resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div className="d-flex justify-content-center" id="resume">
-        <iframe className="py-3" src="https://docs.google.com/document/d/e/2PACX-1vRnjwjZ-gsrCdfL3Axr75dLT52Wjyc9h7Ws1ZMHloOAxay3YXzG606NAeyhj5rOGs5f4Em5LoSeyIaC/pub?embedded=true" type="application/pdf" width="800" height="1000" title="resume"></iframe>
+    <div>
+      <div className="d-flex justify-content-center" id="resume">
+      <a href="https://docs.google.com/document/d/e/2PACX-1vRrj1iQ871ZtSSQzpE0TmEtzpV6jK3sOXm__Kt8ET9GMv9EgBGpbSUR3Bv8M8TL1AUYQcXAWNtULCcn/pub"><iframe className="py-3" src="https://docs.google.com/document/d/e/2PACX-1vRnjwjZ-gsrCdfL3Axr75dLT52Wjyc9h7Ws1ZMHloOAxay3YXzG606NAeyhj5rOGs5f4Em5LoSeyIaC/pub?embedded=true" type="application/pdf" width="800" height="1000" title="resume"></iframe></a>
     </div>
+    <div>
+      <a href="https://drive.google.com/file/d/14Om0uKKCPw8dBiqjZCQSR9y0xP1qXw_P/view?usp=sharing">Download Resume Here</a>
+      </div>
+    </div>
+    
   )
 }
