44b50d927c055848e3154997b1a9ec10e06bd3f2
DrDano <danielharned@gmail.com>
2022-05-06 16:32:37 -0700

    adding portfolioData object with parsed fields for title and description

--- src/API/apiData.js
+++ src/API/apiData.js
@@ -4,13 +4,23 @@ const pinnedProjects = async () => {
     const pinnedData = await pinnedProjectsArr
     const readmeArr = pinnedData.user.pinnedItems.edges.map((project) => {
         return {name: project.node.name, readme: project.node.object.text};
-    })
+    });
 
+    const readmeComponents = readmeArr.map((project) => {
+        return project.readme.split("##");
+    });
 
+    const portfolioData = {
+        title: readmeArr.name,
+        description: readmeComponents[1][1].split("\n").filter((chunk) => {
+            if (chunk.search(/^(.+)\/+([^\/]+)$/)) {
+                return chunk
+            }
+        }),
+        
+    }
+
+    console.log(portfolioData.description)
 }
 
 pinnedProjects()
-
-// const readmeData = pinnedProjects.map((edge) => {
-//     console.log(edge)
-// })
