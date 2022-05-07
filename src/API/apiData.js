55ef4c0ca7931b24ea06177fbd4241c760d21372
DrDano <danielharned@gmail.com>
2022-05-07 16:42:05 -0700

    adding apiData to loop which returns array of projects and their details to match current output of useState in Portfolio component

--- src/API/apiData.js
+++ src/API/apiData.js
@@ -2,48 +2,56 @@ const { pinnedProjectsArr } = require("./githubAPI");
 
 const pinnedProjects = async () => {
   const pinnedData = await pinnedProjectsArr;
-  const readmeArr = pinnedData.user.pinnedItems.edges.map((project) => {
+  const readmeArr = await pinnedData.user.pinnedItems.edges.map((project) => {
     return {
       name: project.node.name,
-      readme: project.node.object.text,
+      readme: project.node.object.text.split("##"),
       url: project.node.url,
     };
   });
 
-  const readmeComponents = readmeArr.map((project) => {
-    return project.readme.split("##");
+  const portfolioProjects = await readmeArr.map((project, i) => {
+    const photoUrl = project.readme[1]
+      .split("\n")[2]
+      .split("(")[1]
+      ?.slice(1, -1) || "";
+
+    const portfolioData = {
+      title: project.name,
+      description: project.readme[1]
+        .split("\n")
+        .filter((chunk) => {
+          if (chunk.search(/^(.+)\/+([^\/]+)$/)) {
+            return chunk;
+          }
+        })
+        .splice(1)
+        .join("")
+        .trim(),
+      technologies: /* list of technologies based on badges */0,
+      photo:
+        "https://raw.githubusercontent.com" +
+        project.url
+          .split("/")
+          .filter((chunk) => chunk === "github")
+          .join("") +
+        "/DrDano/" +
+        project.name +
+        "/main" +
+        photoUrl,
+      projectDeployment: project.readme[3]
+        .split("(")[1]
+        ?.trim()
+        ?.slice(0, -1),
+      key: i,
+      projectRepo: project.url,
+    };
+    return portfolioData;
   });
-  const photoUrl =
-    readmeComponents[0][1].split("\n")[2].split("(")[1].slice(1, -1);
-
-  const portfolioData = {
-    title: readmeArr[0].name,
-    description: readmeComponents[1][1]
-      .split("\n")
-      .filter((chunk) => {
-        if (chunk.search(/^(.+)\/+([^\/]+)$/)) {
-          return chunk;
-        }
-      })
-      .splice(1)
-      .join("")
-      .trim(),
-    photo:
-      "https://raw.githubusercontent.com" +
-      readmeArr[0].url
-        .split("/")
-        .filter((chunk) => chunk === "github")
-        .join("") +
-      "/DrDano/" +
-      readmeArr[0].name +
-      "/main" + photoUrl,
-    projectDeployment: /*--deployment url needs to come from Deployment part of readme */deployment,
-    key: 0,
-    /* key will be incremented with loop */
-    projectRepo: readmeArr[0].url
-  };
-
-  console.log(portfolioData.projectRepo);
+  return portfolioProjects
 };
 
-pinnedProjects();
+(logPinned = async () => {
+  const projects = await pinnedProjects();
+  console.log(projects)
+})();
