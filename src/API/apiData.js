fff7e29f7cca438dfa1be34e54f12830590f5d53
DrDano <danielharned@gmail.com>
2022-05-06 17:10:37 -0700

    adding photo url to portfolioData object, need to change all repo photo names to match their repo names exactly

--- src/API/apiData.js
+++ src/API/apiData.js
@@ -3,7 +3,7 @@ const {pinnedProjectsArr} = require('./githubAPI')
 const pinnedProjects = async () => {
     const pinnedData = await pinnedProjectsArr
     const readmeArr = pinnedData.user.pinnedItems.edges.map((project) => {
-        return {name: project.node.name, readme: project.node.object.text};
+        return {name: project.node.name, readme: project.node.object.text, url: project.node.url};
     });
 
     const readmeComponents = readmeArr.map((project) => {
@@ -11,16 +11,18 @@ const pinnedProjects = async () => {
     });
 
     const portfolioData = {
-        title: readmeArr.name,
+        title: readmeArr[0].name,
         description: readmeComponents[1][1].split("\n").filter((chunk) => {
             if (chunk.search(/^(.+)\/+([^\/]+)$/)) {
                 return chunk
             }
-        }),
-        
+        }).splice(1).join("").trim(),
+        // parsed out title and description from readme, now need to parse out photo url (will go to photo in actual repository)
+        // Need to change all readme photo names to match their repo names exactly
+        photo: "https://raw.githubusercontent.com" + readmeArr[0].url.split("/").filter((chunk)=>chunk==="github").join("") + "/DrDano/" + readmeArr[0].name + "/main/src/assets/images/" + readmeArr[0].name + ".jpg",
     }
 
-    console.log(portfolioData.description)
+    console.log(portfolioData.photo)
 }
 
 pinnedProjects()
