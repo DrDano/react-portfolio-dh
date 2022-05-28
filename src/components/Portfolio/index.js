a7563657f78319cb2f10526dca876a0a43c16d19
DrDano <danielharned@gmail.com>
2022-05-28 15:45:02 -0700

    altering flex spacing of project components

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,7 +10,8 @@ export default function Portfolio() {
         "https://raw.githubusercontent.com/DrDano/react-portfolio-dh/main/src/assets/images/react-portfolio-dh.jpg",
       projectDeployment: "https://drdano.github.io/react-portfolio-dh/",
       projectRepo: "https://github.com/DrDano/react-portfolio-dh",
-      technologies: "https://img.shields.io/badge/javascript-100-yellow?logo=javascript",
+      technologies:
+        "https://img.shields.io/badge/javascript-100-yellow?logo=javascript",
       title: "react-portfolio-dh",
     },
   ]);
@@ -26,30 +27,23 @@ export default function Portfolio() {
     <div id="project-container">
       {projects.map((p) => (
         <div className="py-3" id="project-internals" key={`portfolio${p.key}`}>
-          <h2 key={`h${p.key}`}>
-            {p.title}
-          </h2>
-          <div key={`div${p.key}`} className="m-2 d-flex project p-2">
+          <h2 key={`h${p.key}`}>{p.title}</h2>
+          <div key={`div${p.key}`} className="m-2 project p-2">
+            <div>
             <p key={`p${p.key}`} className="project-des mx-2 p-2">
               {p.description}
             </p>
-            {p.projectDeployment ? (<div>
+            {p.projectDeployment ? (
+              <div className="project-content">
                 <a
-                href={p.projectDeployment}
-                rel="noopener noreferrer"
-                target="_blank"
-                className="text-decoration-none project-header"
+                  href={p.projectDeployment}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="text-decoration-none"
                 >
-                Project Deployment
+                  Project Deployment
                 </a>
-              </div>) : <div className="text-dark">Project is not Deployed</div>}
-            <img
-              name="project-image"
-              src={p.photo}
-              alt={p.title}
-              key={`img${p.key}`}
-            />
-            <a
+                <a
               href={p.projectRepo}
               rel="noopener noreferrer"
               target="_blank"
@@ -58,11 +52,26 @@ export default function Portfolio() {
             >
               Project GitHub Repository
             </a>
-          </div>
-          <div>
-            {<div>{[...p.technologies].map((p) => {
-              return <img src={p}></img>
-            })}</div>}
+              </div>
+            ) : (
+              <div className="text-dark">Project is not Deployed</div>
+            )}
+            </div>
+            <img
+              name="project-image"
+              src={p.photo}
+              alt={p.title}
+              key={`img${p.key}`}
+            />
+            <div>
+              {
+                <div className="project-technologies">
+                  {[...p.technologies].slice(0, p.technologies.length - 1).map((p) => {
+                    return <div className="tech-used"><img src={p} className="tech-used-img"></img></div>;
+                  })}
+                </div>
+              }
+            </div>
           </div>
         </div>
       ))}
