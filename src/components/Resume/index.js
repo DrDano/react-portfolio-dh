29adac9ac54fbd10243ac5051e3ec21b7a41f1b9
DrDano <danielharned@gmail.com>
2022-05-30 20:51:20 -0700

    adding screen size dependency to resume width

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div id="resume-container">
       <div className="d-flex justify-content-center" id="resume">
-      <iframe className="py-3-sm w-100" src="https://docs.google.com/document/d/e/2PACX-1vRrj1iQ871ZtSSQzpE0TmEtzpV6jK3sOXm__Kt8ET9GMv9EgBGpbSUR3Bv8M8TL1AUYQcXAWNtULCcn/pub?embedded=true" type="application/pdf" title="resume"></iframe>
+      <iframe className="py-3-sm w-100-sm" src="https://docs.google.com/document/d/e/2PACX-1vRrj1iQ871ZtSSQzpE0TmEtzpV6jK3sOXm__Kt8ET9GMv9EgBGpbSUR3Bv8M8TL1AUYQcXAWNtULCcn/pub?embedded=true" type="application/pdf" title="resume"></iframe>
     </div>
     <div>
       <a href="https://docs.google.com/document/d/1zDYxeqiuctxX8lKPY44np3iUR1HGdWglGgCx5zP_Soc/edit?usp=sharing">Download Resume Here</a>
