77437dfa9ead6db13e7225d78f202dfa9843c36f
DrDano <danielharned@gmail.com>
2022-06-19 18:54:06 -0700

    rearranging routes in server/app.js

--- server/app.js
+++ server/app.js
@@ -6,30 +6,33 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config()
 
-const home = require('./routes/index')
+const routes = require('./routes')
 
 const app = express();
 
-let allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://git.heroku.com/portfolio-dh.git', 'https://portfolio-dh.herokuapp.com/'];
+// let allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://git.heroku.com/portfolio-dh.git', 'https://portfolio-dh.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-app.use('/', home);
+app.use(routes);
 
 
 module.exports = app;
