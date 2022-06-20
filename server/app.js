680886f67b8b76196134d0295b629a4931d87a49
DrDano <danielharned@gmail.com>
2022-06-19 18:45:24 -0700

    routing all to home route

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config()
 
-const ghApi = require("./routes/API/apiData");
+const home = require('./routes/index')
 
 const app = express();
 
@@ -28,22 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api', ghApi);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/', home);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
