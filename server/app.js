c829c79113593e6310a66e2df05bdc6572df78ba
DrDano <danielharned@gmail.com>
2022-06-20 19:02:20 -0700

    altering static resources path

--- server/app.js
+++ server/app.js
@@ -1,27 +1,14 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
 
 const routes = require('./routes')
 
 const app = express();
-
-// let allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://git.heroku.com/portfolio-dh.git', 'https://portfolio-dh.herokuapp.com/'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,10 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 }
 
 app.use(routes);
 
-
-module.exports = app;
+app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
