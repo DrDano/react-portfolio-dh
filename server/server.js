819f9ede61da48a380eff515b90c9f6ecde29187
DrDano <danielharned@gmail.com>
2022-06-26 08:26:39 -0700

    offloading excess packages that are not necessary at the moment, altering font color to improve accessibility score

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const PORT = process.env.PORT || 3001;
@@ -12,7 +11,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 
 const root = path.join(__dirname, '../client/build')
